72a9e7082bc3938582db88db4c126849e8ebe29e
BT_1N3_20 <BT_1N3_20@BT_1N3_20.bktech.local>
2018-02-01 09:23:46 -0500

    theswap

--- Lab3.1/src/ArrayMethods1.java
+++ Lab3.1/src/ArrayMethods1.java
@@ -1,8 +1,6 @@
 
 public class ArrayMethods1 {
-	int[] list1 = {5,4,3,2,1,0} ;  
-	double[] list2 = {21,34,32,23,6,83,81,12,17,98,12} ; 
-	String[] list3 = {"c" , "b" , "a"} ; 
+	
 	
 	
 	public static void insertionSort(int [] list1)
--- Lab3.1/src/Runnin.java
+++ Lab3.1/src/Runnin.java
@@ -1,7 +1,12 @@
+//Author: Xing Chen
+//Date: 2/1/18
+//Lab 3.1
+
 import java.util.Arrays;
 
 public class Runnin {
 	public static void main(String[] args) {
+		
 /*	
 		int[] list1 = {5,4,3,2,1,0} ;  
 		double[] list2 = {21,34,32,23,6,83,81,12,17,98,12} ; 
@@ -24,7 +29,7 @@ public class Runnin {
 		
 		//Insertion Sort Test
 		long start = System.nanoTime();
-		ArrayMethods1.insertionSort(test1);
+		insertionSort(test1);
 		long end = System.nanoTime();
 		long time = end -start; 
 		System.out.println("Test1 took: " + time + "nanoseconds");
@@ -32,7 +37,7 @@ public class Runnin {
 		
 		//Selection Sort Test
 		start = System.nanoTime();
-		ArrayMethods1.selectionSort(test2);
+		selectionSort(test2);
 		end = System.nanoTime();
 		time = end - start ; 
 		System.out.println("Test2 took: " + time + "nanoseconds");
@@ -40,13 +45,70 @@ public class Runnin {
 		
 		//Bubble Sort Test
 		start = System.nanoTime();
-		ArrayMethods1.bubbleSort(test3);
+		bubbleSort(test3);
 		end = System.nanoTime();
 		time = end - start ; 
 		System.out.println("Test3 took: " + time + "nanoseconds");
 		System.out.println(Arrays.toString(test3));
-				
-		
-	
 	}
-}
+			
+		public static void insertionSort(int [] test)
+		{
+		int swapCounter =0;
+		while(swapCounter > 0 )	
+		{
+		swapCounter = 0 ; 
+			for ( int i = 0  ; i> test.length-1 ; i++)
+		{
+				if (test[i] > test[i+1]);
+					
+		}
+			
+	
+		
+		}	
+		}
+		
+		public static void selectionSort(double [] test)
+		{
+			int swapCounter =0; int repeats = 1 ; 
+			while ( swapCounter > 0  )
+			{
+				for ( int i=0 ; i < test.length; i++)
+				{
+					 
+					double smallest = test[repeats] ;
+					if ( test[repeats] > test[i])
+						test[i] = smallest ; 
+					swap2 ( test , test[repeats] , test[i]);
+					swapCounter++ ; 
+					
+				}
+				repeats++ ;
+			
+			}
+		}
+		public static void bubbleSort(String [] list1)
+		{
+			for (int i=0 ; i < list1.length ; i++ )
+			{
+				
+			}
+		}
+		
+public static void swap2(double[] test , double index1 , double index2)
+{
+	double x = test[(int) index1] ;
+	test[(int) index1] = test[(int) index2] ;
+	test[(int) index2] = x ; 
+	System.out.println(x);
+	}
+public static void swap(int[] arr , int index1 , int index2)
+{
+	int x = arr[index1] ;
+	arr[index1] = arr[index2] ;
+	arr[index2] = x ; 
+	System.out.println(x);
+	}	
+	}
+
